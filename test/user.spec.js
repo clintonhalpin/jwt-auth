9f0f6d18fa6a6c1c62ed8b127578b6e1c1b79a99
Clinton Halpin <clintonhalpin@gmail.com>
2015-09-17 18:49:21 -0400

    YOLO

--- test/user.spec.js
+++ test/user.spec.js
@@ -6,19 +6,18 @@ var shutdown = require('../server').shutdown;
 
 describe("API", function(){
     var url = "http://localhost:" + port + "/";
-
     before(function () {
       boot();
     });
 
-    it("POST to /sessions/create w/o creds WON'T work", function(done) {
+    it("should fail POST to sessions/create", function(done) {
       request.post(url + 'sessions/create', function(error, response, body) {
         expect(response.statusCode).to.equal(400);
         done();
       });
     });
 
-    it("POST to /sessions/create with valid creds works", function(done) {
+    it("should succeed POST sessions/create with valid credentials", function(done) {
       var options = {
         url: url + 'sessions/create',
         form: {
@@ -26,18 +25,31 @@ describe("API", function(){
           password: 'gonto'
         }
       };
-
       request.post(options, function(error, response, body) {
-        expect(body.id_token);
+        expect(JSON.parse(body).id_token).to.be.a('string');;
         expect(response.statusCode).to.equal(201);
         done();
       });
 
     });
 
+    it("should succeed POST to users with valid credentials", function(done) {
+      var options = {
+        url: url + 'users',
+        form: {
+          username: 'yolo',
+          password: 'yolo'
+        }
+      };
+      request.post(options, function(error, response, body) {
+        expect(JSON.parse(body).id_token).to.be.a('string');;
+        expect(response.statusCode).to.equal(201);
+        done();
+      });
 
-    it("GET /api/protected with valid creds works!", function(done) {
-      
+    });
+
+    it("should succeed GET api/protected with valid credentials", function(done) {
       var options = {
         url: url + 'sessions/create',
         form: {
@@ -45,16 +57,13 @@ describe("API", function(){
           password: 'gonto'
         }
       };
-
       request.post(options, function(error, response, body) {
-
         var authorizedURL = {
           url: url + 'api/protected',
           headers: {
             Authorization: 'Bearer ' + JSON.parse(body).id_token
           }
         }
-
         request.get(authorizedURL, function(error, response, body){
           expect(response.statusCode).to.equal(200);
           done();
